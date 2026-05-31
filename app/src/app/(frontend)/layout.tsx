d9578a1785a140be0a8a1a1e8ca901ef6f1db213
Mark Musil <mark.r.musil@gmail.com>
2026-05-31 09:23:14 -0400

    fix duplication of @payloadcs/ui inside storage-s3. Removed conflicting rootlayout between front and admin panel.

--- app/src/app/(frontend)/layout.tsx
+++ app/src/app/(frontend)/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from 'next'
-import './globals.css'
+import '../globals.css'
 
 export const metadata: Metadata = {
   title: 'Mark Musil',
   description: 'Small business website',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function FrontendLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
